b0fa2d4dde8229321560bb114293d8fa3b86ec20
JcaiYang <2252811595@qq.com>
2020-05-22 22:07:24 +0800

    第一次提交

---
+++ src/components/goods/GoodsDesc.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="goodsdesc-container">
+    <h3>{{ info.title }}</h3>
+    <hr>
+    <div class="content" v-html="info.content"></div>
+    <div class="img_wrap">
+    <img :src="info.src"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import goodsdescjson from "../../json/goodsdesc.json";
+export default {
+  data() {
+    return {
+      info: {} // 图文数据
+    };
+  },
+  created() {
+    this.getGoodsDesc();
+  },
+  methods: {
+    getGoodsDesc() {
+    //   this.$http
+    //     .get("api/goods/getdesc/" + this.$route.params.id)
+    //     .then(result => {
+    //       if (result.body.status === 0) {
+    //         this.info = result.body.message[0];
+    //       }
+    //     });
+    goodsdescjson.message.forEach((v,i)=>
+    {
+        if(v.id==this.$route.params.id){
+            console.log(v);
+            this.info=v;
+            console.log(this.info.src);
+        }
+    });
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.goodsdesc-container {
+  padding: 4px;
+  h3 {
+    font-size: 16px;
+    color: #226aff;
+    text-align: center;
+    margin: 15px 0;
+  }
+  .content{
+      margin:8px auto;
+    img{
+      width: 100%;
+    }
+  }
+  .img_wrap{
+      display:flex;
+      justify-content:center;
+      align-items:center;
+      img{
+          width:100%;
+      }
+  }
+}
+</style>
---
+++ src/components/tabbar/SearchContainer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="sbtn">
+    <input placeholder="请输入您要搜索的商品" class="sbtn_text"/>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+body{
+  width:100%;
+  height:100%;
+  padding:10px;
+}
+.sbtn{
+height: 60rpx;
+display: flex;
+margin: 5px 0;
+}
+.sbtn_text{
+    background-color: #fff;
+flex: 1;
+height: 100%;
+padding-left: 30rpx;
+padding:8px;
+text-align:center;
+
+}
+</style>
